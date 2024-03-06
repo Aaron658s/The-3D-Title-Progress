349fc7a2a530d098555c71947ae6e44e15bc479e
Aaron658s <148803881+Aaron658s@users.noreply.github.com>
2024-03-06 17:27:14 -0500

    Update Charactermovement.cs

--- Charactermovement.cs
+++ Charactermovement.cs
@@ -2,17 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Charactermovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 5.0f;
+    public float jumpForce = 5.0f;
+    public bool isOnGround = true;
+    private float horizontalInput;
+    private float forwardInput;
+    private Rigidbody playerRb;
+    private Rigidbody rb;
+
     void Start()
     {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward*Time.deltaTime*speed*forwardInput);
+        transform.Translate(Vector3.right*Time.deltaTime*speed*horizontalInput);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up*jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+    }
+
+    void Awake()
+    {
+        Debug.Log("Awake called.");
+    }
+
+    void FixedUpdate()
+    {
+        Debug.Log("FixedUpdate time :" + Time.deltaTime);
     }
 }
